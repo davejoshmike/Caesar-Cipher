5d20e230d0eb7e8d7aefdc4c1772b0a4d482a1e6
davejoshmike <davejoshmike@gmail.com>
2016-10-03 13:24:33 -0400

    Revised Client Code/Documentation

--- CaesarCipherClient.java
+++ CaesarCipherClient.java
@@ -1,31 +1,61 @@
-//import java.util.Random; //Random
 import java.net.Socket; //socket
 import java.net.UnknownHostException; //socket
+import java.io.IOException; //socket
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException; //socket
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+/** The Caesar Cipher Client Class uses the Java Sockets and Java IO libraries to create two-way encrypted traffic.
+ *
+ * After taking a rotational number between 0 and 25 'n' from the server the client will:
+ * 	1. read input from the user, and encrypt the message before sending it off to the server
+ * 	2. listen for a response from the server, unencrypting and displaying the server's message
+ *
+ * Caesar Cipher Client Algorithm (TL;DR):
+ * 	1.Read user input (stdIn)
+ *	2.Send input to server (out)
+ * 	3.Receive message back from server (in.readLine())
+ * 	4a.For the first message,
+ *     		Set Cipher rotation value (n) from the received message
+ *			(Note: n must be between 1 and 25)
+ * 	4b.For all messages after,
+ * 		Unencrypt and display the received message
+ * 	5.Repeat Steps 1-4b
+ * 	6a.If errors,
+ * 		print stack trace
+ * 	6b.If user or server exit,
+ * 		close all sockets
+ *
+ * Correct usage (hostname, port of server):
+ *      java CaesarCipherClient [hostname] [port]
+ * i.e. java CaesarCipherClient 127.0.0.1 49152
+ *
+ * Note: Must be running the Caesar Cipher Server
+ *
+ * @author David Michel djm43
+ * @date 11 May 2016
+ * @version 03 Oct 2016
+ *
+ */
 public class CaesarCipherClient {
-	//private static char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' }; => also didn't work well
-	//private static alphabet = "abcdefghijklmnopqrstuvwxyz"; //=>indicies of String are not constant. sometimes a is index 24
-
 	public static void main(String[] args) {
+		//Read in hostname and port of which to connect
 		if(!(args.length>0 && args.length<3)){
 			System.err.println("Correct Use: \n\tjava CaesarCipherClient [hostname] [port]");
 			return;
 		}
 		String hostname = args[0];
 		int port = Integer.parseInt(args[1]);
+
 		Socket MyClient = null;
 		BufferedReader in = null;
 		BufferedReader stdIn = null;
 		DataOutputStream output = null;
 		DataInputStream input = null;
 		PrintWriter out;
-		int n = 0; //default (has to be initialized)
+		int n = 0; //default (needs to be initialized)
 		boolean firstMsg = true;
 		String inputStr = "";
 		String encryptedMsg = "";
@@ -49,20 +79,19 @@ public class CaesarCipherClient {
 			out = new PrintWriter(MyClient.getOutputStream(), true);
 			in = new BufferedReader(new InputStreamReader(MyClient.getInputStream()));
 			stdIn = new BufferedReader(new InputStreamReader(System.in));
-			//handle io exception
 			inputStr = null;
-			while((inputStr = stdIn.readLine()) != null){ //read user input from command line
-				out.println(inputStr); //send user input to the server
-				encryptedMsg = in.readLine(); //receive message back from the server
+
+			while((inputStr = stdIn.readLine()) != null){
+				out.println(inputStr);
+				encryptedMsg = in.readLine();
 				if(firstMsg){
-					//set the rotation number, n
 					try{
 					n = Integer.parseInt(encryptedMsg);
 					} catch(NumberFormatException e){
 						System.err.println("Error: Invalid rotation value");
 						System.exit(-1);
 					}
-					if(n<=0 && n>=25){
+					if(n<=0 && n>25){
 						System.err.println("Error: Rotation value must be between 1 and 25");
 						System.exit(-1);
 					}
@@ -75,16 +104,6 @@ public class CaesarCipherClient {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-
-		////////////////////////////////////////////////////////
-
-		//Get input from user
-
-		//do cipher
-
-		//server should replay with that same number i.e. if(!(n = serverresponse.n)) drop; //or resend??
-
-		//close sockets
 		try {
 			output.close();
 			input.close();
@@ -94,48 +113,20 @@ public class CaesarCipherClient {
 		}
 	}
 
+	/* decodeMsg() will decrypt a message encoded with Casear-Cipher type encryption,
+	 * given the encrypted message and its rotational value of which to decrypt with
+	 *
+	 * @param: encryptedMsg, the message to decrypt
+	 * @param: weight, the rotational value (n)
+	 * @return: unencryptedMsg
+	 */
 	public static String decodeMsg(String encryptedMsg, int weight){
 		String unencryptedMsg = "";
 		int index;
 		for(int i = 0; i<encryptedMsg.length();i++){
-			/*
-			//attempt 1 w/ alphabetStr
-			if(alphabet.contains(encryptedMsg.substring(i,i+1))){ //charAt() and isLetter()  giving me errors
-				if(verbose){
-					System.out.println("Index (before): " + alphabet.indexOf(encryptedMsg.substring(i,i+1)));
-				}
-				index = Math.abs(alphabet.indexOf(encryptedMsg.substring(i,i+1))-weight%25);
-				if(verbose){
-					System.out.println("Index (after): "+ index);
-				}
-				unencryptedMsg += alphabet.substring(index,index+1);
-			} else {
-				unencryptedMsg += encryptedMsg.charAt(i);
-			}
-
-			//attempt 2 with char[] alphabet
-			if(new String(alphabet).contains(encryptedMsg.substring(i,i+1))){ //charAt() and isLetter()  giving me errors
-				index = new String(alphabet).indexOf(encryptedMsg.substring(i,i+1));
-				if(verbose){
-					System.out.println("Index (before): " + index);
-				}
-				//if(index<0){
-					//index = 25-Math.abs(index);
-				//}else {
-					index = Math.abs((index-weight));
-				//}
-				if(verbose){
-					System.out.println("Index (after): "+ index);
-				}
-				unencryptedMsg += alphabet[index];
-			} else {
-				unencryptedMsg += encryptedMsg.charAt(i);
-			}
-			 */
-
-			//attempt 3
 			if(Character.isLetter(encryptedMsg.charAt(i))){
 				index = (int)(encryptedMsg.charAt(i));
+
 				//for lowercase (ASCII characters a:97 to z:122)
 				if((index-weight) <= 96 && index > 96 && index < 123){
 					unencryptedMsg += (char)(122 - (96-(index-weight)));
@@ -143,6 +134,7 @@ public class CaesarCipherClient {
 				else if(index > 96 && index < 123){
 					unencryptedMsg += (char)(index-weight);
 				}
+
 				//for uppercase (ASCII characters A:65 to Z:90)
 				if((index-weight) <= 64 && index > 64 && index < 91){
 					unencryptedMsg += (char)(90 - (64 -(index-weight)));
@@ -152,10 +144,7 @@ public class CaesarCipherClient {
 				}
 			} else
 				unencryptedMsg += encryptedMsg.charAt(i);
-			//System.out.print("From: " + encryptedMsg.charAt(i) + ": " + (int)encryptedMsg.charAt(i));
-			//System.out.println(" To: " + unencryptedMsg.charAt(i) + ": " + (int)unencryptedMsg.charAt(i));
 		}
 		return unencryptedMsg;
 	}
 }
-
